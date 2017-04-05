9be2c473c6612876f4ac8eba43170d1f25ab6dfa
Ian <iantennyson@gmail.com>
2017-04-05 15:11:42 +0100

    moving on to styling my cocktail page

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,15 @@ ingredient3.save()
 ingredient4 = Ingredient.new({'name' => 'Orange', 'price_per_ltr' => 1.23, 'is_alcoholic' => false, 'quantity' => 15, 'cost_price' => 0.0, 'sale_price' => 0.0, 'profit' => 0.0, 'mark_up' => 6})
 ingredient4.save()
 
+ingredient5 = Ingredient.new({'name' => 'Whiskey', 'price_per_ltr' => 15.0, 'is_alcoholic' => true, 'quantity' => 5, 'cost_price' => 0.0, 'sale_price' => 0.0, 'profit' => 0.0, 'mark_up' => 0})
+ingredient5.save()
+
+ingredient6 = Ingredient.new({'name' => 'Angostura Bitters', 'price_per_ltr' => 49.80, 'is_alcoholic' => true, 'quantity' => 3, 'cost_price' => 0.0, 'sale_price' => 0.0, 'profit' => 0.0, 'mark_up' => 1})
+ingredient6.save()
+
+ingredient7 = Ingredient.new({'name' => 'Simple Syrup', 'price_per_ltr' => 6.70, 'is_alcoholic' => false, 'quantity' => 6, 'cost_price' => 0.0, 'sale_price' => 0.0, 'profit' => 0.0, 'mark_up' => 2})
+ingredient7.save()
+
 ingredient10 = Ingredient.new({'name' => 'Bombay Saphire', 'price_per_ltr' => 10.0, 'is_alcoholic' => true, 'quantity' => 10, 'cost_price' => 0.0, 'sale_price' => 0.0, 'profit' => 0.0, 'mark_up' => 10})
 ingredient10.save()
 
@@ -35,35 +44,34 @@ ingredient10.save()
 
 
 
-cocktail1 = Cocktail.new({'name' => 'Sex On The Beach', 'prep_description' => 'Shake all four ingredients, serve over ice in a slim jim garnish with a orange slice.'})
+cocktail1 = Cocktail.new({'name' => 'Smoked Old Fashioned', 'prep_description' => 'Add your ingredients into a mixing glass and stir for several minutes. Fill karaf with smoke and strain in your old fashioned. Swirl and pour into a rocks glass with a large cube of ice. Garnish with an orange peel.'})
 cocktail1.save()
 
 cocktail2 = Cocktail.new({'name' => 'Woo Woo', 'prep_description' => 'Shake the three ingredients and serve over ice in a slim jim, garnish with a wedge of lime'})
 cocktail2.save()
 
 
-#Sex on the Beach
-ingredients_list1 = IngredientList.new({'cocktail_id' => cocktail1.id, 'ingredient_id' => ingredient1.id, 'measure' => 25})
+#Smoked Old Fashioned
+ingredients_list1 = IngredientList.new({'cocktail_id' => cocktail1.id, 'ingredient_id' => ingredient5.id, 'measure' => 50})
 ingredients_list1.save()
 
-ingredients_list2 = IngredientList.new({'cocktail_id' => cocktail1.id, 'ingredient_id' => ingredient2.id, 'measure' => 25})
+ingredients_list2 = IngredientList.new({'cocktail_id' => cocktail1.id, 'ingredient_id' => ingredient6.id, 'measure' => 5})
 ingredients_list2.save()
 
-ingredients_list3 = IngredientList.new({'cocktail_id' => cocktail1.id, 'ingredient_id' => ingredient3.id, 'measure' => 50})
+ingredients_list3 = IngredientList.new({'cocktail_id' => cocktail1.id, 'ingredient_id' => ingredient7.id, 'measure' => 15})
 ingredients_list3.save()
 
-ingredients_list4 = IngredientList.new({'cocktail_id' => cocktail1.id, 'ingredient_id' => ingredient4.id, 'measure' => 50})
-ingredients_list4.save()
+
 
 #Woo Woo
-ingredients_list5 = IngredientList.new({'cocktail_id' => cocktail2.id, 'ingredient_id' => ingredient1.id, 'measure' => 25})
+ingredients_list4 = IngredientList.new({'cocktail_id' => cocktail2.id, 'ingredient_id' => ingredient1.id, 'measure' => 25})
+ingredients_list4.save()
+
+ingredients_list5 = IngredientList.new({'cocktail_id' => cocktail2.id, 'ingredient_id' => ingredient2.id, 'measure' => 25})
 ingredients_list5.save()
 
-ingredients_list6 = IngredientList.new({'cocktail_id' => cocktail2.id, 'ingredient_id' => ingredient2.id, 'measure' => 25})
+ingredients_list6 = IngredientList.new({'cocktail_id' => cocktail2.id, 'ingredient_id' => ingredient3.id, 'measure' => 100})
 ingredients_list6.save()
-
-ingredients_list7 = IngredientList.new({'cocktail_id' => cocktail2.id, 'ingredient_id' => ingredient3.id, 'measure' => 100})
-ingredients_list7.save()
 
 
 
